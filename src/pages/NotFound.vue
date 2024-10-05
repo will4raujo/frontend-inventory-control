a5a008360b1232b90989f03ecf6f96280bfcc4dc
willian <willian.araujo1994@gmail.com>
2024-10-05 19:43:29 -0300

    feat: not found layout

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,9 +1,22 @@
 <template>
-  <h1>404 - Página não encontrada</h1>
+  <div class="not-found">
+    <Frown :size="64" />
+    <h1>404 - Página não encontrada</h1>
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'NotFound',
-};
+<script setup>
+import { Frown } from 'lucide-vue-next';
 </script>
+
+<style scoped>
+  .not-found {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+    color: #FFF;
+  }
+</style>
