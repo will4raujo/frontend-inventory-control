486398bb03261d6af120326d4d72b03e8a5842d3
willian <willian.araujo1994@gmail.com>
2024-10-05 16:18:40 -0300

    feat: App pages layout
    - new component li to navigation
    - new layout from app pages

---
+++ src/components/atoms/Li.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { House, Users, Barcode, ChartBarStacked, Truck, LogOut } from 'lucide-vue-next';
+
+const props = defineProps<{ to: string, label: string }>();
+
+const IconComponent = computed(() => {
+  switch (props.to) {
+    case '/':
+      return House;
+    case '/users':
+      return Users;
+    case '/products':
+      return Barcode;
+    case '/categories':
+      return ChartBarStacked;
+    case '/suppliers':
+      return Truck;
+    case '/logout':
+      return LogOut;
+    default:
+      return House;
+  }
+});
+</script>
+
+<template>
+  <li>
+    <component :is="IconComponent" :size="20"  class="li-icon" />
+    <router-link :to="props.to">
+      {{ props.label }}
+    </router-link>
+  </li>
+</template>
+
+<style scoped>
+  li {
+    position: relative;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    height: 40px;
+
+  }
+
+  li:hover {
+    background-color: #444;
+  }
+
+  .li-icon {
+    position: absolute;
+    left: 1.5rem;
+  }
+
+  li a {
+    position: absolute;
+    color: #fff;
+    text-decoration: none;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    padding-left: 3rem;
+    padding-top: 8px;
+  }
+
+</style>
--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,15 +1,87 @@
 <template>
-  <div>
-    <h1>Auth Layout</h1>
-    <RouterView />
+  <div class="app-container">
+    <aside class="menu-aside">
+      <nav>
+        <ul>
+          <Li to="/" label="Home" />
+          <Li to="/products" label="Produtos" />
+          <Li to="/categories" label="Categorias" />
+          <Li to="/users" label="Usuários" />
+          <Li to="/suppliers" label="Fornecedores" />
+        </ul>
+
+        <button class="logout-button">
+          <LogOut :size="20" />
+          Sair
+        </button>
+      </nav>
+    </aside>
+    <main class="main-content">
+      <RouterView />
+    </main>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'AppLayout',
-}
+<script setup>
+
+  import { House, Users, Barcode, ChartBarStacked, Truck, LogOut } from 'lucide-vue-next';
+  import Li from '../components/atoms/li.vue';
+
 </script>
 
 <style>
+  .app-container {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 1.25rem;
+    min-height: 100vh;
+    width: 100vw;
+    margin: 0 auto;
+    position: relative;
+  }
+
+  .menu-aside {
+    background-color: #333;
+    color: #fff;
+    padding: 1rem;
+    height: 100vh;
+    width: 180px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    transition: all 0.3s;
+    padding: 1.5rem 0;
+  }
+
+  .menu-aside nav {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  .main-content {
+    position: relative;
+    padding: 1rem;
+    margin-left: 190px;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .logout-button {
+    background-color: #333;
+    color: #fff;
+    border: none;
+    padding: 0.5rem 1rem;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    cursor: pointer;
+    margin-right: 6rem;
+  }
+
+  .logout-button:hover {
+    color: #f00;
+  }
 </style>
