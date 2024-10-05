f61dc388919318079142ff40d5f82afcff05feca
willian <willian.araujo1994@gmail.com>
2024-10-05 19:44:26 -0300

    feat: suppliers page layout

--- src/pages/Suppliers.vue
+++ src/pages/Suppliers.vue
@@ -1,10 +1,88 @@
+<script setup>
+  import Button from '../components/atoms/Button.vue';
+</script>
+
 <template>
   <h1>Fornecedores</h1>
+  <div>
+    <div class="first-row">
+      <div class="inputs-container">
+        <div class="btn-wrapper">
+          <Button text="Novo fornecedor" />
+        </div>
+      </div>
+    </div>
+    <div class="table-wrapper">
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Contato</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Nome do fornecedor</td>
+            <td>(44) 99999-9999</td>
+            <td>
+              <Button text="Editar" />
+              <Button text="Excluir" />
+            </td>
+          </tr>
+          <tr>
+            <td>Nome do fornecedor</td>
+            <td>(44) 99999-9999</td>
+            <td>
+              <Button text="Editar" />
+              <Button text="Excluir" />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'Suppliers',
-};
+<style scoped>
+  .first-row {
+    margin: 2rem 0;
+  }
 
-</script>
+  .inputs-container {
+    display: flex;
+    align-items: end;
+    gap: 1rem;
+  }
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+
+  th {
+    background-color: #333;
+    color: #fff;
+    padding: 0.5rem;
+  }
+
+  td {
+    border-bottom: 1px solid #666;
+    padding: 0.5rem;
+  }
+  
+
+  th:last-child {
+    text-align: center;
+  }
+
+  td:last-child {
+    display: flex;
+    gap: 1rem;
+    justify-content: end;
+  }
+
+  tr:hover {
+    background-color: #555;
+  }
+  </style>
