95d976440ec349aad9a137a761ee5041de4bcdd4
willian <willian.araujo1994@gmail.com>
2024-10-05 19:44:41 -0300

    feat: users page layout

--- src/pages/Users.vue
+++ src/pages/Users.vue
@@ -1,9 +1,92 @@
+<script setup>
+  import Button from '../components/atoms/Button.vue';
+</script>
+
 <template>
   <h1>Usuários</h1>
+  <div>
+    <div class="first-row">
+      <div class="inputs-container">
+        <div class="btn-wrapper">
+          <Button text="Novo usuário" />
+        </div>
+      </div>
+    </div>
+    <div class="table-wrapper">
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Nome do usuário</td>
+            <td>email@example.com</td>
+            <td>
+              <Button text="Editar" />
+              <Button text="Excluir" />
+            </td>
+          </tr>
+          <tr>
+            <td>Nome do usuário</td>
+            <td>email@example.com</td>
+            <td>
+              <Button text="Editar" />
+              <Button text="Excluir" />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'Users',
-};
-</script>
+<style scoped>
+  .first-row {
+    margin: 2rem 0;
+  }
+
+  .inputs-container {
+    display: flex;
+    align-items: end;
+    gap: 1rem;
+  }
+
+  .btn-wrapper {
+    max-width: 220px;
+  }
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+
+  th {
+    background-color: #333;
+    color: #fff;
+    padding: 0.5rem;
+  }
+
+  td {
+    border-bottom: 1px solid #666;
+    padding: 0.5rem;
+  }
+  
+
+  th:last-child {
+    text-align: center;
+  }
+
+  td:last-child {
+    display: flex;
+    gap: 1rem;
+    justify-content: end;
+  }
+
+  tr:hover {
+    background-color: #555;
+  }
+  </style>
