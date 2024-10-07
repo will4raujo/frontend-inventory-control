9ca8d5c26187a14f5cf0563cb155212c17240b37
willian <willian.araujo1994@gmail.com>
2024-10-07 12:51:31 -0300

    feat: products integration

--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -1,19 +1,150 @@
-<script setup>
+<script>
   import Button from '../components/atoms/Button.vue';
   import IconButton from '../components/atoms/IconButton.vue';
   import InputText from '../components/atoms/InputText.vue';
-  
-  import { ref } from 'vue';
   import Modal from '../components/atoms/Modal.vue';
+  import api from '../services/api';
+  import { useUserStore } from '../stores/user';
   
-  const showModal = ref(false);
-
-  function addProduct(event) {
-    event.preventDefault();
-    console.log('Adicionando produto');
-    showModal.value = false;
-  }
- 
+  export default {
+    components: {
+      Button,
+      IconButton,
+      Modal,
+      InputText
+    },
+    data() {
+      return {
+        products: [],
+        categories: [],
+        suppliers: [],
+        showModal: false,
+        isVisible: false,
+        isEditing: false,
+        currentProductId: null,
+        formData: {
+          name: '',
+          code: '',
+          description: '',
+          category_id: '',
+          supplier_id: '',
+          cost_price: '',
+          sale_price: '',
+          stock: '',
+          min_stock: '',
+          max_stock: '',
+          expiration_date: ''
+        }
+      };
+    },
+    mounted() {
+      this.getProducts();
+      this.getCategories();
+      this.getSuppliers();
+    },
+    methods: {
+      async getProducts() {
+        await api.get('/products')
+          .then((response) => {
+            this.products = response.data;
+          })
+          .catch((error) => {
+            console.error(error);
+          });        
+      },
+      async getCategories() {
+        await api.get('/categories')
+          .then((response) => {
+            this.categories = response.data;
+            console.log(this.categories);
+          })
+          .catch((error) => {
+            console.error(error);
+          });        
+      },
+      async getSuppliers() {
+        await api.get('/suppliers')
+          .then((response) => {
+            this.suppliers = response.data;
+          })
+          .catch((error) => {
+            console.error(error);
+          });        
+      },
+      async handleAddProduct(id) {
+        if (this.isEditing) {
+        await api.put(`/products/${this.currentProductId}`, this.formData)
+        .then((response) => {
+            this.getProducts();
+            alert('Produto editado com sucesso!');
+            this.showModal = false;
+            this.clearForm();
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+        } else {
+          await api.post('/products', this.formData)
+            .then((response) => {
+              this.getProducts();
+              alert('Produto cadastrado com sucesso!');
+              this.clearForm();
+              this.showModal = false;
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        }
+      },
+      async handleEditProduct(id) {
+        this.isEditing = true;
+        this.currentProductId = id;
+        await api.get(`/products/${id}`)
+          .then((response) => {
+            this.formData = response.data;
+            this.showModal = true;
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      },
+      async handleDeleteProduct(id) {
+        confirm('Deseja excluir o Produto?') && (
+          await api.delete(`/products/${id}`)
+            .then((response) => {
+              this.getProducts();
+              alert('Produto excluído com sucesso!');
+            })
+            .catch((error) => {
+              console.error(error);
+            })
+        );
+      },
+      clearForm() {
+        this.formData = {
+          name: '',
+          code: '',
+          description: '',
+          category_id: '',
+          supplier_id: '',
+          cost_price: '',
+          sale_price: '',
+          stock: '',
+          min_stock: '',
+          max_stock: '',
+          expiration_date: ''
+        };
+        this.isEditing; 
+        this.currentProductId = null;
+      },
+      handleCancel() {
+        confirm('Deseja cancelar?') && (
+          this.showModal = false,
+          this.clearForm()
+        );
+      }
+    },
+  };
 </script>
 
 <template>
@@ -21,7 +152,9 @@
   <div>
     <div class="first-row">
       <div class="inputs-container">
-        <Button text="Novo produto" @click="showModal = true" />
+        <div class="btn-wrapper">  
+          <Button text="Novo produto" @click="showModal = true" />
+        </div>
       </div>
     </div>
     <div class="table-wrapper">
@@ -29,28 +162,25 @@
         <thead>
           <tr>
             <th>Nome</th>
-            <th>Contato</th>
+            <th>Código</th>
+            <th>Quantidade</th>
+            <th>Preço de venda</th>
             <th>Ações</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Nome do fornecedor</td>
-            <td>(44) 99999-9999</td>
-            <td>
-              <div>
-                <IconButton iconType="pen" />
-                <IconButton iconType="trash" />
-              </div>
-            </td>
+          <tr v-if="products.length === 0">
+            <td colspan="5" style="text-align: center;">Nenhum produto cadastrado</td>
           </tr>
-          <tr>
-            <td>Nome do fornecedor</td>
-            <td>(44) 99999-9999</td>
+          <tr v-else v-for="product in products" :key="product.id">
+            <td>{{ product.name }}</td>
+            <td>{{ product.code }}</td>
+            <td>{{ product.stock }}</td>
+            <td>{{ product.sale_price }}</td>
             <td>
               <div>
-                <IconButton iconType="pen" />
-                <IconButton iconType="trash" />
+                <IconButton iconType="pen" @click="handleEditProduct(product.id)" />
+                <IconButton iconType="trash" @click="handleDeleteProduct(product.id)" />
               </div>
             </td>
           </tr>
@@ -58,11 +188,45 @@
       </table>
     </div>
   </div>
-  <Modal :isVisible="showModal" @close="showModal = false">
-    <h2>Novo produto</h2>
-    <form class="form-content" @submit.prevent="addProduct">
+  <Modal :isVisible="showModal" @close="handleCancel">
+    <h2>{{ isEditing ? 'Editar produto' : 'Novo produto' }}</h2>
+    <form class="form-content" @submit.prevent="handleAddProduct">
+      <div class="input-wrapper">
+        <div class="inputs-container">
+          <InputText v-model="formData.name" label="Nome" id="name" placeholder="Nome do produto" type="text" />
+          <InputText v-model="formData.code" label="Código" id="code" placeholder="Código do produto" type="text" />
+        </div>
+        <div class="inputs-container">
+          <div class="select-wrapper">
+          <label for="category">Categoria</label>
+          <select v-model="formData.category_id" label="Categoria" id="category">
+            <option value="" disabled selected>Selecione uma categoria</option>
+            <option v-for="category in categories" :key="category.id" :value="category.id">{{ category.name }}</option>
+          </select>
+        </div>
+        <div class="select-wrapper">
+          <label for="supplier">Fornecedor</label>
+          <select v-model="formData.supplier_id" label="Fornecedor" id="supplier">
+            <option value="" disabled selected>Selecione um fornecedor</option>
+            <option v-for="supplier in suppliers" :key="supplier.id" :value="supplier.id">{{ supplier.name }}</option>
+          </select>
+        </div>
+      </div>
+      <div class="inputs-container">  
+        <InputText v-model="formData.cost_price" label="Preço de custo" id="costPrice" type="number" placeholder="Preço de custo" />
+        <InputText v-model="formData.sale_price" label="Preço de venda" id="salePrice" type="number" placeholder="Preço de venda" />
+      </div>
+      <div class="inputs-container">
+        <InputText v-model="formData.stock" label="Quantidade" id="stock" type="number" placeholder="Quantidade" />
+        <InputText v-model="formData.min_stock" label="Estoque mínimo" id="minStock" type="number" placeholder="Estoque mínimo" />
+        <InputText v-model="formData.max_stock" label="Estoque máximo" id="maxStock" type="number" placeholder="Estoque máximo" />
+      </div>
+      <InputText v-model="formData.expiration_date" label="Data de validade" id="expirationDate" type="date" placeholder="Data de validade" />
+      <textarea v-model="formData.description" label="Descrição" id="description" placeholder="Descrição do produto" class="textarea"></textarea>
+      </div>
+      
       <div class="btn-wrapper">
-        <Button text="Cancelar" styleType="ghost" @click="showModal = false" />
+        <Button text="Cancelar" styleType="ghost" @click="handleCancel" />
         <Button text="Salvar" @click="addProduct" />
       </div>
     </form>
@@ -70,16 +234,20 @@
 </template>
 
 <style scoped>
-  .first-row {
-    margin: 2rem 0;
-  }
+  .textarea {
+    height: 100px;
+    width: 100%;
+    margin-top: 1rem;
+    background-color: #555;
+    border-radius: 5px;
+    border: none;
+    padding: .8rem;
+    color: #fff;
 
-  .inputs-container {
-    display: flex;
-    align-items: end;
-    justify-content: end;
-    gap: 1rem;
-    max-width: 220px;
+    resize: none;
+    box-sizing: border-box;
   }
-  
-  </style>
+  .textarea::placeholder {
+    color: #ccc;
+  }
+</style>
