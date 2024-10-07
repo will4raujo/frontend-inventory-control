986a534b54dbb94a51ec50ddb49f5b3954123774
willian <willian.araujo1994@gmail.com>
2024-10-06 21:06:59 -0300

    fix: conflict merge

--- src/pages/Suppliers.vue
+++ src/pages/Suppliers.vue
@@ -1,6 +1,111 @@
-<script setup>
+<script>
   import Button from '../components/atoms/Button.vue';
   import IconButton from '../components/atoms/IconButton.vue';
+  import Modal from '../components/atoms/Modal.vue';
+  import InputText from '../components/atoms/InputText.vue';
+  import api from '../services/api';
+
+  export default {
+      components: {
+        Button,
+        IconButton,
+        Modal,
+        InputText
+      },
+      data() {
+        return {
+          suppliers: [],
+          showModal: false,
+          isVisible: false,
+          isEditing: false,
+          currentSupplierId: null,
+          formData: {
+            name: '',
+            cnpj: '',
+            contact: '',
+          }
+        };
+      },
+      mounted() {
+        this.getSuppliers();
+      },
+      methods: {
+        async getSuppliers() {
+          await api.get('/suppliers')
+            .then((response) => {
+              this.suppliers = response.data;
+              console.log(this.suppliers);
+            })
+            .catch((error) => {
+              console.error(error);
+            });        
+        },
+        async handleAddSupplier(id) {
+          if (this.isEditing) {
+          await api.put(`/suppliers/${this.currentSupplierId}`, this.formData)
+          .then((response) => {
+              this.getSuppliers();
+              this.showModal = false;
+              alert('Fornecedor editado com sucesso!');
+              this.clearForm();
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+          } else {
+            await api.post('/suppliers', this.formData)
+              .then((response) => {
+                this.getSuppliers();
+                this.showModal = false;
+                alert('Fornecedor cadastrado com sucesso!');
+                this.clearForm();
+              })
+              .catch((error) => {
+                console.error(error);
+              });
+          }
+        },
+        async handleEditSupplier(id) {
+          this.isEditing = true;
+          this.currentSupplierId = id;
+          await api.get(`/suppliers/${id}`)
+            .then((response) => {
+              this.formData = response.data;
+              this.showModal = true;
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        },
+        async handleDeleteSupplier(id) {
+          confirm('Deseja excluir o fornecedor?') && (
+            await api.delete(`/suppliers/${id}`)
+              .then((response) => {
+                this.getSuppliers();
+                alert('Fornecedor excluído com sucesso!');
+              })
+              .catch((error) => {
+                console.error(error);
+              })
+          );
+        },
+        clearForm() {
+          this.formData = {
+            name: '',
+            cnpj: '',
+            contact: '',  
+          };
+          this.isEditing; 
+          this.currentUserId = null;
+        },
+        handleCancel() {
+          confirm('Deseja cancelar?') && (
+            this.showModal = false,
+            this.clearForm()
+          );
+        }
+      },
+  };
 </script>
 
 <template>
@@ -9,7 +114,7 @@
     <div class="first-row">
       <div class="inputs-container">
         <div class="btn-wrapper">
-          <Button text="Novo fornecedor" />
+          <Button text="Novo fornecedor" @click="showModal = true" />
         </div>
       </div>
     </div>
@@ -18,28 +123,23 @@
         <thead>
           <tr>
             <th>Nome</th>
+            <th>Cnpj</th>
             <th>Contato</th>
             <th>Ações</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Nome do fornecedor</td>
-            <td>(44) 99999-9999</td>
-            <td>
-              <div>
-                <IconButton iconType="pen" />
-                <IconButton iconType="trash" />
-              </div>
-            </td>
+          <tr v-if="suppliers.length === 0">
+            <td colspan="4" style="text-align: center;">Nenhum fornecedor encontrado.</td>
           </tr>
-          <tr>
-            <td>Nome do fornecedor</td>
-            <td>(44) 99999-9999</td>
+          <tr v-else v-for="supplier in suppliers" :key="supplier.id">
+            <td>{{ supplier.name }}</td>
+            <td>{{ supplier.cnpj }}</td>
+            <td>{{ supplier.contact }}</td>
             <td>
               <div>
-                <IconButton iconType="pen" />
-                <IconButton iconType="trash" />
+                <IconButton iconType="pen" @click="handleEditSupplier(supplier.id)" />
+                <IconButton iconType="trash" @click="handleDeleteSupplier(supplier.id)" />
               </div>
             </td>
           </tr>
@@ -47,17 +147,18 @@
       </table>
     </div>
   </div>
+  <Modal :isVisible="showModal" @close="showModal = false">
+      <h2>{{ isEditing ? 'Editar' : 'Cadastrar' }} fornecedor</h2>
+      <form @submit.prevent="handleAddSupplier" class="form-content">
+        <div class="inputs-container">
+          <InputText v-model="formData.name" label="Nome" placeholder="Nome" type="text" />
+          <InputText v-model="formData.cnpj" label="Cnpj" placeholder="Cnpj" type="text" />
+        </div>
+        <InputText v-model="formData.contact" label="Contato" placeholder="Contato" type="text" />
+        <div class="btn-wrapper">
+          <Button text="Cancelar" @click="handleCancel" />
+          <Button text="Salvar" @click="handleAddSupplier" />
+        </div>
+      </form>
+  </Modal>
 </template>
-
-<style scoped>
-  .first-row {
-    margin: 2rem 0;
-  }
-
-  .inputs-container {
-    display: flex;
-    align-items: end;
-    gap: 1rem;
-  }
-
-  </style>
