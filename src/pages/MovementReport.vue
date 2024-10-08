ef2d4386c29be3417aaf5fe567d4110efedb59ac
willian <willian.araujo1994@gmail.com>
2024-10-08 13:56:03 -0300

    feat: balance report and movement report

--- src/pages/MovementReport.vue
+++ src/pages/MovementReport.vue
@@ -1,8 +1,55 @@
-<script setup>
+<script>
+import Button from '../components/atoms/Button.vue';
+import api from '../services/api';
+
+export default {
+  components: {
+    Button,
+  },
+  data() {
+    return {
+      movements: [],
+      activeList: 'all',
+    };
+  },
+  mounted() {
+    this.getProductsBalance();
+  },
+  methods: {
+    async getProductsBalance(type = '') {
+      let url = '/lists/movements';
+      if (type && type !== 'all') {
+        url += `?type=${type}`;
+      }
+
+      await api.get(url)
+        .then((response) => {
+          this.movements = response.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+    setActiveList(type) {
+      this.activeList = type;
+      this.getProductsBalance(type);
+    },
+  },
+};
 </script>
 
 <template>
   <h1>Relatório de movimentação de estoque</h1>
+  <div class="first-row">
+
+    <select @change="setActiveList($event.target.value)">
+      <option value="all">Todas as movimentações</option>
+      <option value="input">Entradas</option>
+      <option value="output">Saídas</option>
+      <option value="loss">Perdas</option>
+      <option value="adjustment">Ajustes</option>
+    </select>
+  </div>
   <div>
     <div class="table-wrapper">
       <table>
@@ -15,17 +62,14 @@
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Nome do produto</td>
-            <td>10</td>
-            <td>Entrada</td>
-            <td>01/01/2021</td>
+          <tr v-if="movements.length === 0">
+            <td colspan="4">Nenhuma movimentação encontrada</td>
           </tr>
-          <tr>
-            <td>Nome do produto</td>
-            <td>10</td>
-            <td>Saída</td>
-            <td>01/01/2021</td>
+          <tr v-else v-for="movement in movements" :key="movement.id">
+            <td>{{ movement.product_name }}</td>
+            <td>{{ movement.quantity }}</td>
+            <td>{{ movement.type }}</td>
+            <td>{{ new Date(movement.date).toLocaleDateString() }}</td>
           </tr>
         </tbody>
       </table>
@@ -36,6 +80,10 @@
 <style scoped>
   h1 {
     margin-bottom: 2rem;
+  }
+
+  h3 {
+    margin: 2rem 0 1rem;
   }
 
   table {
@@ -53,14 +101,12 @@
     border-bottom: 1px solid #666;
     padding: 0.5rem;
   }
-  
 
   th:last-child, tr {
     text-align: center;
   }
 
-
   tr:hover {
     background-color: #555;
   }
-  </style>
+</style>
